7792e9af26c22c507ccd641c9bdda6d6d4b5185d
parasshah143 <parasshah25@gmail.com>
2023-03-22 10:51:04 +0530

    Appium

--- demo1_android/demo1_app_install.py
+++ demo1_android/demo1_app_install.py
@@ -1,6 +1,7 @@
 import time
 
 from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
 
 des_cap = {
     "platformName": "android",
@@ -9,11 +10,30 @@ des_cap = {
     "udid": "emulator-5554"
 }
 
-driver = webdriver.Remote(command_executor="http://127.0.0.1:4723/wd/hub", desired_capabilities=des_cap)
+driver = webdriver.Remote(command_executor="http://localhost:4723/wd/hub", desired_capabilities=des_cap)
+driver.implicitly_wait(30)
 
-print(driver.page_source)
+driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Dismiss']").click()
+
+driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Sign in']").click()
+
+driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Sign in']").click()
+
+driver.find_element(AppiumBy.XPATH,
+                    "//android.widget.EditText[@content-desc='Enter an e-mail address or username']").send_keys("dina")
+driver.find_element(AppiumBy.XPATH, "//android.widget.EditText[contains(@content-desc,'Pass')]").send_keys("dina123")
+
+# click on sign in
+driver.find_element(AppiumBy.XPATH, "(//android.widget.TextView[@text='Sign in'])[2]").click()
+# get the text "There was an issue signing in" and print it
+
+actual_error = driver.find_element(AppiumBy.XPATH, "//*[contains(@text,'issue')]").text
+
+print(actual_error)
+
+actual_error = driver.find_element(AppiumBy.XPATH, "//*[contains(@text,'issue')]").get_attribute("text")
+
+print(actual_error)
 
 time.sleep(5)
-
 driver.quit()
-
