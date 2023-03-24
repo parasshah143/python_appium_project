eb5e973c7f895c2445c8b57bb0aa488bbd9a8ba7
parasshah143 <parasshah25@gmail.com>
2023-03-24 11:16:04 +0530

    Appium

--- demo1_android/demo3_open_khan_app.py
+++ demo1_android/demo3_open_khan_app.py
@@ -1,16 +1,22 @@
+import time
+
 import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
-
+""" Open APP"""
 class AppiumConfig:
     @pytest.fixture(scope="function", autouse=True)
     def handle_app_launch(self):
         des_cap = {
             "platformName": "android",
             "deviceName": "oneplus",
-            "app": r"C:\paras_shah\python_scripts\appium_project\khan-academy-7-3-2.apk",
-            "udid": "emulator-5554"
+            "appPackage": "org.khanacademy.android",
+            "appActivity":"org.khanacademy.android.ui.library.MainActivity",
+            "noReset":True
+            # "udid":"emulator-5554"
         }
 
         self.driver = webdriver.Remote(command_executor="http://localhost:4723/wd/hub", desired_capabilities=des_cap)
@@ -18,14 +24,17 @@ class AppiumConfig:
         yield
         self.driver.quit()
 
-
 class TestAndroidDeviceLocal(AppiumConfig):
     def test_invalid_login(self):
-        self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Dismiss']").click()
+
+        #presence of element - using length
+        if len(self.driver.find_elements(AppiumBy.XPATH,"//android.widget.TextView[@text='Dismiss']"))>0:
+            self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Dismiss']").click()
+
         self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Sign in']").click()
         self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Sign in']").click()
         self.driver.find_element(AppiumBy.XPATH,
-                                 "//android.widget.EditText[@content-desc='Enter an e-mail address or username']").send_keys(
+                            "//android.widget.EditText[@content-desc='Enter an e-mail address or username']").send_keys(
             "dina")
         self.driver.find_element(AppiumBy.XPATH, "//android.widget.EditText[contains(@content-desc,'Pass')]").send_keys(
             "dina123")
--- demo1_android/test_demo2_app_install.py
+++ demo1_android/test_demo2_app_install.py
@@ -6,31 +6,32 @@ from appium.webdriver.common.appiumby import AppiumBy
 des_cap = {
     "platformName": "android",
     "deviceName": "oneplus",
-    "app":  r"C:\paras_shah\python_scripts\appium_project\khan-academy-7-3-2.apk",
-    # "udid":"emulator-5557"
+    "app": r"C:\paras_shah\python_scripts\appium_project\khan-academy-7-3-2.apk",
+    "udid": "emulator-8888"
 }
 
-driver=webdriver.Remote(command_executor="http://localhost:4723/wd/hub",desired_capabilities=des_cap)
+driver = webdriver.Remote(command_executor="http://localhost:4723/wd/hub", desired_capabilities=des_cap)
 driver.implicitly_wait(30)
 
-driver.find_element(AppiumBy.XPATH,"//android.widget.TextView[@text='Dismiss']").click()
+driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Dismiss']").click()
 
-driver.find_element(AppiumBy.XPATH,"//android.widget.TextView[@text='Sign in']").click()
+driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Sign in']").click()
 
-driver.find_element(AppiumBy.XPATH,"//android.widget.TextView[@text='Sign in']").click()
+driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Sign in']").click()
 
-driver.find_element(AppiumBy.XPATH,"//android.widget.EditText[@content-desc='Enter an e-mail address or username']").send_keys("dina")
-driver.find_element(AppiumBy.XPATH,"//android.widget.EditText[contains(@content-desc,'Pass')]").send_keys("dina123")
+driver.find_element(AppiumBy.XPATH,
+                    "//android.widget.EditText[@content-desc='Enter an e-mail address or username']").send_keys("dina")
+driver.find_element(AppiumBy.XPATH, "//android.widget.EditText[contains(@content-desc,'Pass')]").send_keys("dina123")
 
-#click on sign in
-driver.find_element(AppiumBy.XPATH,"(//android.widget.TextView[@text='Sign in'])[2]").click()
-#get the text "There was an issue signing in" and print it
+# click on sign in
+driver.find_element(AppiumBy.XPATH, "(//android.widget.TextView[@text='Sign in'])[2]").click()
+# get the text "There was an issue signing in" and print it
 
-actual_error=driver.find_element(AppiumBy.XPATH,"//*[contains(@text,'issue')]").text
+actual_error = driver.find_element(AppiumBy.XPATH, "//*[contains(@text,'issue')]").text
 
 print(actual_error)
 
-actual_error=driver.find_element(AppiumBy.XPATH,"//*[contains(@text,'issue')]").get_attribute("text")
+actual_error = driver.find_element(AppiumBy.XPATH, "//*[contains(@text,'issue')]").get_attribute("text")
 
 print(actual_error)
 
